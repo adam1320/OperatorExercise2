eb0116d770eaf64f1034a8de97acf9275d287962
Adam Brannon <adam1320@rocketmail.com>
2022-01-13 14:31:05 -0500

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace OperatorExercise2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is the radius of your circle?");
+            var radius = double.Parse(Console.ReadLine());
+            double answer=AreaOfCircle(radius);
+            Console.WriteLine($"The area of a circle with a radius of {radius} is {answer}");
+
+
+        }
+
+        //Create method
+        public static double AreaOfCircle(double radius)
+        {
+            double AreaOfCircle = Math.PI * radius * radius;
+            return AreaOfCircle;
+        }
+        
+        
+
+    }
+}
